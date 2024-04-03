1cb759be8746a1065aabcd886e0e53a7a7ed0670
moeinpourhamzeh <moein.phz@gmail.com>
2024-04-03 11:45:28 +0330

    ADD: Gateway service added

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903ed9af5495e36bd713326919388e8a478f9021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d9e099cbfcea120f3304b6f9a90263de996d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
