d2cb9dc7148c413fbd2b37d4699e9a437ea35624
moeinpourhamzeh <moein.phz@gmail.com>
2024-08-15 01:02:53 +0330

    Docker files added

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb759be8746a1065aabcd886e0e53a7a7ed0670")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbcb1efdc061aa7ca1b77e9008ea6cb0b3e9d71")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
